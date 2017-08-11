d98656da783f7a8177adceb1454ec83762e4f8b9
Jackson Ming Hu <huming2207@gmail.com>
2017-08-11 23:39:37 +1000

    Generator & UI: Implement main methods and CJK filter

--- java/DataSourceTester/src/com/cyxhm/StdInputFileGenerator.java
+++ java/DataSourceTester/src/com/cyxhm/StdInputFileGenerator.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class StdInputFileGenerator
 {
-    public static void SeparatedChineseCharsFromText(String inputPath, String outputPath,
+    public static void SeparatedEastAsianCharsFromText(String inputPath, String outputPath,
                                               boolean randomDelete) throws IOException
     {
         System.out.println("[INFO] Running test: SeparatedChineseCharsFromText()");
@@ -19,6 +19,7 @@ public class StdInputFileGenerator
         // Current line of the string
         String lineRead;
 
+
         while ((lineRead = bufferedReader.readLine()) != null)
         {
             // Increase the line counter
@@ -26,7 +27,7 @@ public class StdInputFileGenerator
             System.out.println(String.format("[INFO] Reading/writing line %d...", lineCount));
 
             // Convert a line of string to char array
-            char[] charArray = lineRead.toCharArray();
+            char[] charArray = easternAsianStrFilter(lineRead).toCharArray();
 
             // ...then write every char as command (append "A" before the char) one by one
             for (char selectedChar : charArray)
@@ -117,5 +118,44 @@ public class StdInputFileGenerator
         bufferedWriter.close();
     }
 
+    // ONLY EAST ASIAN chars  will reserved after this filter method is applied...
+    // This includes:
+    //      Chinese (simplified & traditional stantards used in Mainland/Hong Kong/Macau/Taiwan)
+    //      Korean
+    //      Japanese (including Han chars of course)
+    //      Vietnamese (Han-Nom chars)
+    private static String easternAsianStrFilter(String inputStr)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // Iterate the string by each char inside
+        for(char chineseChar : inputStr.toCharArray())
+        {
+            // Put char into UTF-16 block
+            Character.UnicodeBlock unicodeBlock = Character.UnicodeBlock.of(chineseChar);
+
+            // Append if this char is an Eastern-Asian char
+            if(
+                    // For the details abouut these stuff below, please refer to this article:
+                    // https://en.wikipedia.org/wiki/CJK_Unified_Ideographs
+                    unicodeBlock == Character.UnicodeBlock.CJK_COMPATIBILITY ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_COMPATIBILITY_FORMS ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_RADICALS_SUPPLEMENT ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_STROKES ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_C ||
+                    unicodeBlock == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_D)
+            {
+                stringBuilder.append(chineseChar);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
 
 }
--- java/DataSourceTester/src/com/cyxhm/Tester.java
+++ java/DataSourceTester/src/com/cyxhm/Tester.java
@@ -1,10 +1,116 @@
 package com.cyxhm;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Tester
 {
 
     public static void main(String[] args)
     {
 
+    }
+
+    private static void mainMenu()
+    {
+        System.out.println("Data generator for AA Assignment 1 \nBy Ming Hu (s3554025) & Yuxuan Cheng (s3516930)");
+        System.out.println("Select a test method:\n" +
+                "1. Test by CJK/CJKV (East Asian) chars\n" +
+                "2. Test by repeating English sentences\n" +
+                "3. Test by English words\n\n");
+
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        scanner.nextLine(); // Consumes new line char
+
+        // TODO: 1. Shorten this switch-case; 2. Implements method #2's random deletion
+        switch(userInput)
+        {
+            case 1:
+            {
+                // Gather some information...
+                System.out.print("[NOTICE] Enter article text file input path: ");
+                String inputPath = scanner.nextLine();
+                System.out.print("\n[NOTICE] Enter output text file input path: ");
+                String outputPath = scanner.nextLine();
+                System.out.print("[NOTICE] Enter \"yes\" to generate random deletion command,\n" +
+                        "[NOTICE] or press enter to continue: ");
+                boolean generateRandomDelCmd = scanner.nextLine().toLowerCase().equals("yes");
+
+                // Run generator
+                try
+                {
+                    StdInputFileGenerator.SeparatedEastAsianCharsFromText(inputPath, outputPath, generateRandomDelCmd);
+                }
+                catch (IOException ioError)
+                {
+                    // If error occurs, exit by status code 1 with error message shown
+                    System.err.println("[ERROR] IO Exception thrown! Message shows below: \n\n");
+                    ioError.printStackTrace();
+                    System.exit(1);
+                }
+
+                break;
+            }
+            case 2:
+            {
+                // Gather some information...
+                System.out.print("[NOTICE] Enter repeat times: ");
+                long repeatTimes = Long.valueOf(scanner.nextLine());
+                System.out.print("\n[NOTICE] Enter output text file input path: ");
+                String outputPath = scanner.nextLine();
+                System.out.print("[NOTICE] Enter \"yes\" to generate random deletion command,\n" +
+                        "[NOTICE] or press enter to continue: ");
+                boolean generateRandomDelCmd = scanner.nextLine().toLowerCase().equals("yes");
+
+                // Run generator
+                try
+                {
+                    StdInputFileGenerator.RepeatEnglishSentence(outputPath, repeatTimes);
+                }
+                catch (IOException ioError)
+                {
+                    // If error occurs, exit by status code 1 with error message shown
+                    System.err.println("[ERROR] IO Exception thrown! Message shows below: \n\n");
+                    ioError.printStackTrace();
+                    System.exit(1);
+                }
+
+                break;
+            }
+            case 3:
+            {
+                // Gather some information...
+                System.out.print("[NOTICE] Enter article text file input path: ");
+                String inputPath = scanner.nextLine();
+                System.out.print("\n[NOTICE] Enter output text file input path: ");
+                String outputPath = scanner.nextLine();
+                System.out.print("[NOTICE] Enter \"yes\" to generate random deletion command,\n" +
+                        "[NOTICE] or press enter to continue: ");
+                boolean generateRandomDelCmd = scanner.nextLine().toLowerCase().equals("yes");
+
+                // Run generator
+                try
+                {
+                    StdInputFileGenerator.SeparatedEnglishWord(inputPath, outputPath, generateRandomDelCmd);
+                }
+                catch (IOException ioError)
+                {
+                    // If error occurs, exit by status code 1 with error message shown
+                    System.err.println("[ERROR] IO Exception thrown! Message shows below: \n\n");
+                    ioError.printStackTrace();
+                    System.exit(1);
+                }
+
+                break;
+
+            }
+            default:
+            {
+                System.out.println("[ERROR] Wrong selection, please retry or press Ctrl+C to exit.\n");
+                mainMenu();
+                break;
+            }
+        }
     }
 }
